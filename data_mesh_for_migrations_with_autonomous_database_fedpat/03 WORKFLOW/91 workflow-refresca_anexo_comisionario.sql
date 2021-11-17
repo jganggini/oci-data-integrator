8db0514712353cc6635b52f56012850fb97a962a
jganggini <jganggini@outlook.com>
2021-11-17 13:42:03 -0500

    update

--- data_mesh_for_migrations_with_autonomous_database_fedpat/03 WORKFLOW/91 workflow-refresca_anexo_comisionario.sql
+++ data_mesh_for_migrations_with_autonomous_database_fedpat/03 WORKFLOW/91 workflow-refresca_anexo_comisionario.sql
@@ -1,6 +1,5 @@
 CREATE OR REPLACE PROCEDURE obi_workflow.refresca_anexo_comisionario(
-  par_fecha_desde     IN  DATE     DEFAULT SYSDATE,
-  par_fecha_hasta     IN  DATE     DEFAULT SYSDATE)
+  par_fecha_desde     IN  DATE     DEFAULT SYSDATE)
 AS
 BEGIN
    /*---------------------------------------------------------------------------------------------------------------.
@@ -12,7 +11,7 @@ BEGIN
                                                                                                                     |
    --[proceso] Procesa Anexo Comisionario--------------------------------------------------------------------------*/
       pck_contabilidad.procesa_anexo_comisionario(p_fecha_desde => par_fecha_desde,                               --|
-                                                  p_fecha_hasta => par_fecha_hasta);                              --|
+                                                  p_fecha_hasta => trunc(sysdate));                              --|
    --[fin] Step 01-------------------------------------------------------------------------------------------------*/
 END;
 /
